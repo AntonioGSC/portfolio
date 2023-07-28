8e1228fe357a747ad8f95229f4cf09d5b10d1b3f
AntonioGSC <antoniogabrielagsc@gmail.com>
2023-07-28 14:50:37 -0300

    Small Devices Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ import { heroInformation } from '../constants';
 const Hero = () => {
     return (
         <section className='relative w-full h-screen mx-auto'>
-            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex flex-row items-start gap-5`}>
+            <div className={`${styles.paddingX} absolute inset-0 top-[90px] max-w-7xl flex flex-row items-start gap-5`}>
                 <div className='flex flex-col justify-center items-center mt-5'>
                     <div className='w-5 h-5 rounded-full bg-[#915eff]' />
                     <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -23,11 +23,11 @@ const Hero = () => {
                 </div>
             </div>
 
-            <div className='absolute bottom-24 sm:bottom-0 h-1/2 sm:h-2/3 w-full'>
+            <div className='absolute bottom-28 sm:bottom-0 h-1/2 sm:h-2/3 w-full'>
                 <ComputersCanvas />
             </div>
 
-            <div className='absolute xs:bottom-8 bottom-16 w-full flex justify-center items-center'>
+            <div className='absolute xs:bottom-8 bottom-20 w-full flex justify-center items-center'>
                 <a href='#about'>
                     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
                         <motion.dev
